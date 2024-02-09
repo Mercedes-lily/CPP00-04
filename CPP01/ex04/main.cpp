8f8f1291aa36828a9e5267a2b189fb48e97425ab
Valerie St-Pierre <vst-pier@c2r2p9.42quebec.com>
2024-02-09 17:33:52 -0500

    sed

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,6 +1,36 @@
+#include "Files.hpp"
+#include <iostream>
+#include <string>
 
+std::string find_replace(std::string readed, std::string s1, std::string s2)
+{
+	size_t found = 0;
+	
+	while(found != std::string::npos)
+	{
+		found = readed.find(s1, found);
+		if (found != std::string::npos)
+		{
+			readed.erase(found, s1.length());
+			readed.insert(found, s2);
+			found += s2.length();
+		}
+	}
+	return (readed);
+}
 
 int main(int argc, char **argv)
 {
-	
+	std::string readed;
+	std::string newFile = argv[1];
+	newFile += ".replace";
+	if (argc != 4)
+	{
+		std::cout << "mmm... you need 3 arguments -file -string -string";
+		return (0);
+	}
+	std::fstream in(argv[1]);
+	std::ofstream out(newFile);
+	while(getline(in, readed))
+		out << find_replace(readed, argv[2], argv[3]) << std::endl;	
 }
---
+++ CPP01/ex05/Harl.hpp
@@ -0,0 +1,13 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+class HARL_HPP
+{
+	public :
+		void complain( std::string level );
+	private :
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+};
